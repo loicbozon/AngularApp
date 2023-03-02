9fb4560b2ae8f22ac15ccba511848aa09b8efe3f
loicbozon <loic.bozon@ynov.com>
2023-03-02 14:19:55 +0100

    pute

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCB-u5NIO2Wz94cUNWgjtwjP8moARlvp78",
+        authDomain: "moviedevmobile.firebaseapp.com",
+        projectId: "moviedevmobile",
+        storageBucket: "moviedevmobile.appspot.com",
+        messagingSenderId: "705664004556",
+        appId: "1:705664004556:web:dd71f9a636274d3df2ad69",
+        measurementId: "G-JBM684QB71"
+    }
+  };
